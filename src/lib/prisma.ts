e4169dadb2f3cdac9bbccc86bc13b35c91b959b0
Isaac Reis <isaacreisn@gmail.com>
2026-04-23 13:03:38 -0300

    chore: prepare prisma configuration and build scripts for vercel deployment

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -5,7 +5,8 @@ import { Pool } from "pg";
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 function createPrismaClient() {
-  const connectionString = `${process.env.DATABASE_URL}`;
+  const connectionString =
+    process.env.POSTGRES_PRISMA_URL || process.env.DATABASE_URL;
   const pool = new Pool({ connectionString });
   const adapter = new PrismaPg(pool);
 
